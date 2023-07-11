22190f6cf7a97777d78b7235aff243394cd8083c
JanardhanReddy <bommireddy246@gmail.com>
2023-07-11 23:38:35 +0100

    terraform

--- resource/ec2/main.tf
+++ resource/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sample.id]
 
   tags = {
@@ -30,7 +30,6 @@ resource "aws_security_group" "sample" {
   name        = var.Name
 
   ingress {
-    description      = "TLS from VPC"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -45,7 +44,7 @@ resource "aws_security_group" "sample" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "sample"
   }
 }
 
