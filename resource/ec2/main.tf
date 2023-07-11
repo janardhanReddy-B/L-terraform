2ef1b10945046ef3e4775a774f2429f032470bf3
JanardhanReddy <bommireddy246@gmail.com>
2023-07-11 16:13:03 +0100

    terraform

--- resource/ec2/main.tf
+++ resource/ec2/main.tf
@@ -1,12 +1,36 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [aws_security_group.sample.id]
 
   tags = {
     Name = "var.Name"
   }
 }
 
-variable "Name" {
-  default = "Hello"
+variable "Name" {}
+
+
+resource "aws_security_group" "sample" {
+  name        = var.Name
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
--- resource/main.tf
+++ resource/main.tf
@@ -1,4 +1,4 @@
 module "sample" {
   source = "./ec2"
-  name = "frontend"
+  Name = "frontend"
 }
