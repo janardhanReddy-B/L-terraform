fc845fd52b110c38592b5500f0be63906ed6697f
JanardhanReddy <bommireddy246@gmail.com>
2023-07-11 16:00:07 +0100

    ansible

---
+++ resource/ec2/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "var.Name"
+  }
+}
+
+variable "Name" {
+  default = "Hello"
+}
---
+++ resource/main.tf
@@ -0,0 +1,4 @@
+module "sample" {
+  source = "./ec2"
+  name = "frontend"
+}
